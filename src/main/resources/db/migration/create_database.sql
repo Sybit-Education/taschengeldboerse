49dfc045d42ebde3231e3f47d75ab3e588d62801
sybit\ssr <stephan.strittmatter@sybit.de>
2015-10-20 16:55:14 +0200

    automatic creation of database

--- src/main/resources/db/migration/create_database.sql
+++ src/main/resources/db/migration/create_database.sql
@@ -1,16 +1,11 @@
-CREATE DATABASE `taschengeldboerse` /*!40100 DEFAULT CHARACTER SET latin1 */;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  ssr
+ * Created: 20.10.2015
+ */
 
-CREATE USER 'boersenadmin'@'%' IDENTIFIED BY 'admin2015!';
-GRANT ALL PRIVILEGES ON taschengeldboerse.* TO 'boersenadmin'@'%';
-
-CREATE USER 'boerse'@'%' IDENTIFIED BY 'admin2015!';
-GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON taschengeldboerse.* TO 'sybib'@'%';
-
-
-CREATE USER 'boersenadmin'@'localhost' IDENTIFIED BY 'admin2015!';
-GRANT ALL PRIVILEGES ON taschengeldboerse.* TO 'boersenadmin'@'localhost';
-
-CREATE USER 'boerse'@'localhost' IDENTIFIED BY 'admin2015!';
-GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON taschengeldboerse.* TO 'sybib'@'localhost';
-
-FLUSH PRIVILEGES;
+CREATE DATABASE `taschengeldboerse`;

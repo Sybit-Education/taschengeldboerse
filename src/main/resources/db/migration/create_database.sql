183503709891587fc547a2ddf3bffad2beb9111c
sybit\ssr <stephan.strittmatter@sybit.de>
2015-10-20 17:31:10 +0200

    convert to string for easy usage

--- src/main/resources/db/migration/create_database.sql
+++ src/main/resources/db/migration/create_database.sql
@@ -9,3 +9,20 @@
  */
 
 CREATE DATABASE `taschengeldboerse`;
+
+
+
+CREATE USER 'boersenadmin'@'%' IDENTIFIED BY 'admin2015!';
+GRANT ALL PRIVILEGES ON taschengeldboerse.* TO 'boersenadmin'@'%';
+
+CREATE USER 'boerse'@'%' IDENTIFIED BY 'admin2015!';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON taschengeldboerse.* TO 'sybib'@'%';
+
+
+CREATE USER 'boersenadmin'@'localhost' IDENTIFIED BY 'admin2015!';
+GRANT ALL PRIVILEGES ON taschengeldboerse.* TO 'boersenadmin'@'localhost';
+
+CREATE USER 'boerse'@'localhost' IDENTIFIED BY 'admin2015!';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON taschengeldboerse.* TO 'sybib'@'localhost';
+
+FLUSH PRIVILEGES;

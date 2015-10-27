b166da746e78f2ffbd09ea191a59cf1745b6544c
unknown <Schulungsnb01@SCHULUNGSNB.sybit.de>
2015-10-27 15:55:50 +0100

    Erstelle Ort Feld

---
+++ src/main/resources/db/migration/V1_4__Ort.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Schulungsnb01
+ * Created: 27.10.2015
+ */
+ALTER TABLE `taschengeldboerse`.`jobs` 
+ADD COLUMN `ort` VARCHAR(250) NULL AFTER `zusaetzliche_infos`;
+

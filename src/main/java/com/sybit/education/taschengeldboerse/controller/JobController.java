d05dc722cce76a84e80c96ae2d14222ed9695a80
tsa <Toni.Stella@gmx.de>
2015-10-28 12:39:03 +0100

    findAnbieterById method implemented

--- src/main/java/com/sybit/education/taschengeldboerse/controller/JobController.java
+++ src/main/java/com/sybit/education/taschengeldboerse/controller/JobController.java
@@ -1,6 +1,7 @@
 package com.sybit.education.taschengeldboerse.controller;
 
 
+import com.sybit.education.taschengeldboerse.domain.Anbieter;
 import com.sybit.education.taschengeldboerse.domain.Job;
 import com.sybit.education.taschengeldboerse.service.JobsService;
 import java.util.List;
@@ -30,7 +31,8 @@ public class JobController {
     @Autowired
     private JobsService jobService;
     
-    
+    @Autowired
+    private AnbieterService anbieterService;
     /**
      * Liste für die Schüler alle offenen Jobs auf.
      * @param model
@@ -52,10 +54,10 @@ public class JobController {
     @RequestMapping(value = "/schueler/jobs/detail", method = RequestMethod.GET)
     public ModelAndView getJobDetail(@RequestParam("id") final Integer id, final Model model, final HttpServletRequest request) {
         Job job = jobService.findById(id);
-        
+        Anbieter anbieter = anbieterService.findById(job.getAnbieter());
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("job", job);
-
+        modelAndView.addObject("anbieter", anbieter.getName());
         modelAndView.setViewName("job-detail");
 
         return modelAndView;
--- src/main/java/com/sybit/education/taschengeldboerse/domain/Job.java
+++ src/main/java/com/sybit/education/taschengeldboerse/domain/Job.java
@@ -13,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -34,7 +35,7 @@ public class Job implements Serializable {
 
     @Column(name = "bezeichnung")
     private String bezeichnung;
-
+    
     @Column(name = "anbieter")
     private Integer anbieter;
 
